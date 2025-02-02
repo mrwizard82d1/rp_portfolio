77ae3c5823ed9dd646a94f95e71a07d6ab589835
Larry Jones <mrwizard82d1@gmail.com>
2025-02-02 17:00:36 -0600

    Create first, home page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """
+    Return the home page
+    :param request:
+    :return:
+    """
+    return render(request, 'pages/home.html')
